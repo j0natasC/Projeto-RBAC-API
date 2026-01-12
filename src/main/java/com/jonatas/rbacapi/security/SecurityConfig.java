0460d057dc07fc161e21d9c17352fc3887c1a1dd
Jonatas Cavalcante <jonatasben@hotmail.com>
2026-01-12 12:28:05 -0300

    Adiciona anotação @EnableMethodSecurity à configuração de segurança

--- src/main/java/com/jonatas/rbacapi/security/SecurityConfig.java
+++ src/main/java/com/jonatas/rbacapi/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jonatas.rbacapi.security;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
